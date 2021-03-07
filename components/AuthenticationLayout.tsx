6a9c65e2b64fd78e6159563cb6a35d2ff300ee0e
Kilerd <blove694@gmail.com>
2021-03-07 18:51:54 +0800

    feat: optimize ledger choicer

--- components/AuthenticationLayout.tsx
+++ components/AuthenticationLayout.tsx
@@ -1,12 +1,10 @@
 import styles from './layout.module.scss'
 import Nav from "./Nav";
-import LedgerChoicer from "./LedgerChoicer";
 import React from "react";
 
 export default function AuthenticationLayout({children}) {
   return <main>
     <Nav />
-    <LedgerChoicer/>
     {children}
   </main>
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,29 @@
 import Link from "next/link";
-import styles from './layout.module.scss'
 import {useAuth} from "../contexts/auth";
 import React from "react";
+import {userLedger} from "../contexts/ledger";
 
 export default function Nav() {
   const {user} = useAuth();
+  const ledgerContext = userLedger();
+
+  const ledgerChange = (e) => {
+    ledgerContext.changeLedgerId(e.target.value)
+  }
   return (
     <>
       <header>
         <div className="container">
-          <h1 className="logo">Miser</h1>
+
+          <div className="left">
+            <h1 className="logo">Miser</h1>
+            <select onChange={ledgerChange} value={ledgerContext.ledger_id}>
+              {Object.values(ledgerContext.ledgers).map(it => (
+                <option key={it.id} value={it.id}>{it.name}</option>
+              ))}
+
+            </select>
+          </div>
           <nav className="nav">
             <Link href="/dashboard"><a className="link">DashBoard</a></Link>
             <Link href="/transactions"><a>Transactions</a></Link>
@@ -21,26 +35,33 @@ export default function Nav() {
       </header>
       <style jsx>{`
         header {
-        padding: 10px 0;
-        background-color: #fff;
-        border-bottom: 1px solid #eee;
-        position: sticky;
-        top: 0;
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          max-width: 90%;
-          margin: 0 auto;
-          
-          nav {
+          padding: 10px 0;
+          background-color: #fff;
+          border-bottom: 1px solid #eee;
+          position: sticky;
+          top: 0;
+
+          .container {
             display: flex;
-            a {
-            margin-right: 0.25rem;
-            color: black;
-            text-decoration: none;
+            align-items: center;
+            justify-content: space-between;
+            max-width: 90%;
+            margin: 0 auto;
+
+            .left {
+              display: flex;
+              align-items: center;
             }
-          }
+
+            nav {
+              display: flex;
+
+              a {
+                margin-right: 0.25rem;
+                color: black;
+                text-decoration: none;
+              }
+            }
           }
         }
       `}</style>
